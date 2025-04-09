ff38e8f8db5b667267d46ed648a154f98b929243
ellinorgolden <goldenellinor@gmail.com>
2025-04-09 11:25:30 +0200

    changes for mobile friendly styling

--- src/components/Prospector.tsx
+++ src/components/Prospector.tsx
@@ -5,6 +5,7 @@ import profile1Img from '../assets/images/profile1.png';
 import profile2Img from '../assets/images/profile2.png';
 import settingImg from '../assets/images/settings.png';
 import ImageModal from "./imageModal";
+import  "./Prospector.css";
 
 const Prospector = () => {
     return (
@@ -12,12 +13,14 @@ const Prospector = () => {
       <header>
       <h1>Prospector</h1>
       </header>   
-    <ImageModal src={dashboardImg} alt="Dashboard" />
-    <ImageModal src={positionImg} alt="Position feature" />
-    <ImageModal src={PitchImg} alt="Position with pitch" />
-    <ImageModal src={profile1Img} alt="Profile feature" />
-    <ImageModal src={profile2Img} alt="Create a profile" />
-    <ImageModal src={settingImg} alt="Setting feature" />
+      <div className='prospector-content-section'>
+        <ImageModal src={dashboardImg} alt="Dashboard" />
+        <ImageModal src={positionImg} alt="Position feature" />
+        <ImageModal src={PitchImg} alt="Position with pitch" />
+        <ImageModal src={profile1Img} alt="Profile feature" />
+        <ImageModal src={profile2Img} alt="Create a profile" />
+        <ImageModal src={settingImg} alt="Setting feature" />
+      </div>
   </div>
     );
   };
